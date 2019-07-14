9d3b5db47b14b81f4f0734a97c8dca07d467ea16
Yong Liang <yongliang24@gmail.com>
2019-07-13 19:59:11 -0700

    Update ReverseString.js

--- ReverseString.js
+++ ReverseString.js
@@ -1,1 +1,41 @@
 
+// function reverseString(str){
+
+//     let revArray = [];
+//     // Looping from the end 
+//     let strArray = str.split("")
+//     for(let i = strArray.length; i >= 0; i--) { 
+
+//         revArray.push(strArray[i]); 
+//     } 
+      
+//     // Joining the array elements 
+//     return revArray.join(''); 
+// }
+
+function stackReverse(str){
+    let strArray = [];
+
+    for(let eachLetter of str){
+        strArray.push(eachLetter)
+    }
+
+    strArray.reverse();
+
+    return strArray.join("")
+
+}
+
+stackReverse("google amazon")
+
+// function reverseString(str){
+
+//     let strArray = str.split("") 
+// //it's important not to add space on the quotation mark here.
+
+//     strArray = strArray.reverse()
+
+//     return strArray.join("");
+// }
+
+// reverseString("amazon google apple microsoft facebook")
