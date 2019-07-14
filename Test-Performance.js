8f6b315adcb3b939b6bfa03f34cf594ae43c5b72
Yong Liang <yongliang24@gmail.com>
2019-07-14 12:26:41 -0700

    Update Test-Performance.js

--- Test-Performance.js
+++ Test-Performance.js
@@ -1,1 +1,16 @@
 
+//use the following setup to test the time complexity of the code
+//use function performanceTimer() to wrap around the code you are testing
+
+function performanceTimer(){
+  
+  let timestart = performance.now();
+
+//run code blocks here
+
+  let timeend = performance.now();
+
+  return console.log(`Time Elapsed: ${(timestart - timeend) / 1000} seconds`)
+}
+
+performanceTimer()
