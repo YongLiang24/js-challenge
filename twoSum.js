497217cf0dd3f0959c61385e248c10c983d5fdda
Yong Liang <yongliang24@gmail.com>
2019-07-18 05:24:09 -0700

    Update twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -1,1 +1,18 @@
 
+//nested loop solution
+function twoSum(numArray, numTarget)
+{
+ const arraySize = numArray.length;
+
+ for(let i = 0; i < arraySize; i++){
+
+     for(let j= i + 1; j < arraySize; j++){
+
+       if(numTarget - numArray[i] === numArray[j]){
+
+          return `The sum of indices ${i} and ${j} equals ${numTarget}` 
+        }
+     }
+  }
+ return "target not found."
+}
