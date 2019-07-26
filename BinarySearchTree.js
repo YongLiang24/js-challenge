14b2c7d516d89480dc176e6813093f00106fd516
Yong Liang <yongliang24@gmail.com>
2019-07-25 20:42:26 -0700

    Update BinarySearchTree.js

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -31,3 +31,23 @@ class BinarySearchTree{
     }
 }
 
+class Node{
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+//sample for manually create a BST
+var tree = new BinarySearchTree();
+tree.root = new Node(10);
+tree.root.right = new Node(15);
+tree.root.left = new Node(7);
+tree.root.left.right = new Node(9);
+
+console.log(tree)
+
+}
+
+test()
