9206f83dd7957b4d05af3f641e5c899fdb42401a
Yong Liang <yongliang24@gmail.com>
2019-08-01 04:54:08 -0700

    Update FrenqCount01.js

--- FrenqCount01.js
+++ FrenqCount01.js
@@ -1,1 +1,31 @@
 
+// The indexOf() method returns the position of the first occurrence 
+// of a specified value in a string.
+
+// This method returns -1 if the value to search for never occurs.
+
+function findIndex(str){
+    let n = str.indexOf("w");
+
+    return n;
+}
+
+findIndex("w")
+
+
+function same(arr1, arr2){
+    if(arr1.length !== arr2.length){
+        return false;
+    }
+    for(let i = 0; i < arr1.length; i++){
+        let correctIndex = arr2.indexOf(arr1[i] ** 2)
+        if(correctIndex === -1) {
+            return false;
+        }
+        console.log(arr2);
+        arr2.splice(correctIndex,1)
+    }
+    return true;
+}
+
+same([1,2,3,2], [9,1,4,4])
