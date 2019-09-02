fe449a1af27c30ccae6fa36964932754686027df
Yong Liang <yongliang24@gmail.com>
2019-09-01 23:53:00 -0700

    Update duplicateRemoval.js

--- duplicateRemoval.js
+++ duplicateRemoval.js
@@ -1,1 +1,23 @@
 
+//Objective: remove the duplicate values in array1
+let array1 = [1,2,3,3,4,2,5,7,1]
+
+let array2 = []
+
+for(let eachValue of array1){
+    if(array2.indexOf(eachValue) === -1){
+      array2.push(eachValue)
+    }
+}
+
+console.log(array2) //returns [ 1, 2, 3, 4, 5, 7 ]
+
+
+
+
+//Objective: remove the duplicate values in array1
+
+array1 = Array.from(new Set(array1))
+
+
+console.log(array1) //returns [ 1, 2, 3, 4, 5, 7 ]
